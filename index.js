564c3a4c77389dd3718fda47d33e4de0f8c5ca7a
Grégory Duffort <g.duffort@gmail.com>
2023-11-06 12:19:10 +0100

    challenge express 01.1

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const port = 6001;
+const app = require("./app");
+
+const PORT = 6001;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log("Server is listening on ${PORT}");
+  }
+});
+// const port = 6001;
 
 // const welcome = (req, res) => {
 //   res.send("Welcome to Express");
@@ -23,33 +34,33 @@ const port = 6001;
 //     console.error("Error:", err.message);
 //   });
 
-const getMovies = (req, res) => {
-  res.status(200).json(movies);
-};
+// const getMovies = (req, res) => {
+//   res.status(200).json(movies);
+// };
 
-const moviesList = (req, res) => {
-  res.send("Welcome to my favorite movie list");
-};
+// const moviesList = (req, res) => {
+//   res.send("Welcome to my favorite movie list");
+// };
 
-app.get("/", moviesList);
+// app.get("/", moviesList);
 
-app.get("/api/movies", getMovies);
+// app.get("/api/movies", getMovies);
 
-app.get("/api/movies/:id", (req, res) => {
-  const movieId = parseInt(req.params.id);
-  const movie = movies.find((m) => m.id === movieId);
+// app.get("/api/movies/:id", (req, res) => {
+//   const movieId = parseInt(req.params.id);
+//   const movie = movies.find((m) => m.id === movieId);
 
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(404).json({ message: "Not Found" });
-  }
-});
+//   if (movie) {
+//     res.status(200).json(movie);
+//   } else {
+//     res.status(404).json({ message: "Not Found" });
+//   }
+// });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`server is listening on ${port}`);
-  }
-});
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.error("Something bad happened");
+//   } else {
+//     console.log(`server is listening on ${port}`);
+//   }
+// });
